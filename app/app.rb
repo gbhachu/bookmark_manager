2a512c6a4d0f3b58d01535828db6a1aa3de8a825
Basile Koko <basile_koko@hotmail.com>
2017-01-24 11:31:32 +0000

    exercrise 10 creating modular sinatra

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,13 @@
 require 'sinatra/base'
-require './app/models/link'
+require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Homepage'
+  end
+
+  get '/links' do
+    erb :links
   end
 
   # start the server if ruby file executed directly
--- spec/features/bookmark_manager_spec.rb
+++ spec/features/bookmark_manager_spec.rb
@@ -2,7 +2,7 @@
 feature 'view links' do
   scenario 'see list of links on the homepage' do
     Link.create(url: 'http://google.com', title: 'Google Homepage')
-    visit '/'
+    visit '/links'
     expect(page).to have_content ('http://google.com')
   end
 end
