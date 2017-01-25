30117c9db4fe6ba5cf7271d1d49c8a48ddd1de90
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-01-25 16:28:03 +0000

    refractoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,11 @@ class User
   include DataMapper::Resource
 
   property :id,               Serial
-  property :email,            String
+  property :email,            String,     require: true
   property :password_digest,  Text
 
   validates_confirmation_of :password
+  validates_presence_of :email
 
   def password=(password)
     @password = password
---
+++ spec/features/email_validation_spec.rb
@@ -0,0 +1,8 @@
+feature 'Validates Username' do
+  scenario 'user cant sign in without valid email' do
+
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(page).to have_content("Please enter valid email")
+    
+  end
+end
